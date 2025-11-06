71a2db38eb9e10e459368ca830afe6a25a728376
M-612 <mrudula.bandre02@gmail.com>
2025-11-06 20:12:46 +0530

    Update Render

--- backend/server.js
+++ backend/server.js
@@ -24,3 +24,6 @@ app.use("/api/blogs", blogRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("✅ Backend is running successfully on Render!");
+});
